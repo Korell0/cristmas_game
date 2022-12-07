15c5cd82bcf19ad599ff667d2c1f95dccbc686c8
Logfer <molnar.viktor999@gmail.com>
2022-12-07 10:34:58 +0100

    Generate Map, Obstacle

---
+++ cristmas_game/Fields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace cristmas_game
+{
+    class Fields
+    {
+        public string Name;
+        public PictureBox Background;
+        public int Score;
+
+
+        public Fields(Point point, Size size, string name, int score, string background)
+        {
+            Name = name;
+            Score = score;
+
+            Background = new PictureBox();
+            Background.Visible = true;
+            //Background.BackColor = Color.Wheat;
+            Background.Size = size;
+            Background.Location = point;
+
+            if (background == "Black")
+            {
+                Background.BackColor = Color.Black;
+            }
+            if (background == "Wheat")
+            {
+                Background.BackColor = Color.Wheat;
+            }
+        }
+    }
+}
--- cristmas_game/Form1.cs
+++ cristmas_game/Form1.cs
@@ -12,9 +12,65 @@ namespace cristmas_game
 {
     public partial class Form1 : Form
     {
+        static int Size = 31;
+        static int Obstacles = 5;
+        static Fields[,] Map = new Fields[Size,Size];
         public Form1()
         {
             InitializeComponent();
+            this.Width = 2000;
+            this.Height = 1920;
+            GenerateMap();
+            GenerateObstacle();
+
+        }
+
+        private void GenerateObstacle()
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < Obstacles; i++)
+            {
+                int row = r.Next(1, Size - 1);
+                int column = r.Next(1, Size - 1);
+
+                if (Map[row, column].Background.BackColor == Color.Wheat)
+                {
+                    Map[row, column].Background.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void GenerateMap()
+        {
+            int size = 31;
+
+            int startY = 16;
+            int startX = groupBox1.Height - 10 * size + 10;
+
+            for (int row = 0; row < Map.GetLength(0); row++)
+            {
+                for (int column = 0; column < Map.GetLength(1); column++)
+                {
+                    int xPosition = startX + column * size + column;
+                    int yPosition = startY + row * size + row;
+
+                    if (row == 0 || row == Size - 1)
+                    {
+                        Map[row, column] = new Fields(new Point(xPosition,yPosition), new Size(size,size), "Wall", 0, "Black");
+                        this.Controls.Add(Map[row, column].Background);
+                    }
+                    if (column == 0 || column == Size - 1)
+                    {
+                        Map[row, column] = new Fields(new Point(xPosition, yPosition), new Size(size, size), "Wall", 0, "Black");
+                        this.Controls.Add(Map[row, column].Background);
+                    }
+
+                    Map[row, column] = new Fields(new Point(xPosition, yPosition), new Size(size, size), "Cell", 0, "Wheat");
+
+                    this.Controls.Add(Map[row, column].Background);
+                }
+            }
         }
     }
 }
