da4ab8ed6f4b79eac4a02730f4604795e16cb39d
Logfer <molnar.viktor999@gmail.com>
2022-12-08 14:12:54 +0100

    Neighborhoodcheck begin

--- cristmas_game/Form1.cs
+++ cristmas_game/Form1.cs
@@ -12,17 +12,83 @@ namespace cristmas_game
 {
     public partial class Form1 : Form
     {
-        static int Size = 31;
+        static int Size = 15;
         static int Obstacles = 5;
         static Fields[,] Map = new Fields[Size,Size];
+        static SantaSnake Santa = new SantaSnake();
         public Form1()
         {
             InitializeComponent();
-            this.Width = 2000;
-            this.Height = 1920;
+            this.Width = 1500;
+            this.Height = 1000;
             GenerateMap();
             GenerateObstacle();
+            GenerateSantaSnake();
 
+        }
+
+        private void GenerateSantaSnake()
+        {
+            Random n = new Random();
+
+            int randomrow = n.Next(1, Size - 1);
+            int randomcolumn = n.Next(1, Size - 1);
+
+            if (Map[randomrow,randomcolumn].Name == "Obstacle" && !NeighborhoodCheck(randomrow,randomcolumn))
+            {
+                GenerateSantaSnake();
+            }
+            else
+            {
+                Map[randomrow, randomcolumn].Background.BackColor = Color.Blue;
+            }
+
+
+        }
+
+        private bool NeighborhoodCheck(int row, int column)
+        {
+            //Down
+
+            for (int sor = row; sor > row - 2; sor--)
+            {
+                if (Map[sor,column].Name == "Obstacle" || Map[sor,column].Name == "Wall")
+                {
+                    return false;
+                }
+            }
+
+            //Up
+
+            for (int sor = row; sor < row + 2; sor++)
+            {
+                if (Map[sor, column].Name == "Obstacle" || Map[sor, column].Name == "Wall")
+                {
+                    return false;
+                }
+            }
+
+            //Right
+
+            for (int oszlop = column; oszlop < column + 2; oszlop++)
+            {
+                if (Map[row,oszlop].Name == "Obstacle" || Map[row,oszlop].Name == "Wall")
+                {
+                    return false;
+                }
+            }
+
+            //Left
+
+            for (int oszlop = column; oszlop > column - 2; oszlop--)
+            {
+                if (Map[row, oszlop].Name == "Obstacle" || Map[row, oszlop].Name == "Wall")
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private void GenerateObstacle()
@@ -36,6 +102,7 @@ namespace cristmas_game
 
                 if (Map[row, column].Background.BackColor == Color.Wheat)
                 {
+                    Map[row, column].Name = "Obstacle";
                     Map[row, column].Background.BackColor = Color.Red;
                 }
             }
